248f1237e3a237ed647976fc8fea0e38c966247c
adremarie <adreanne.marie@gmail.com>
2016-02-11 20:34:45 +0000

    lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -17,6 +17,7 @@ function initializePage() {
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
+
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
@@ -27,7 +28,21 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+	$.get("project/" + idNumber, addProject);
+	console.log("project/" + idNumber);
 }
+
+
+function addProject(result) {
+	console.log(result);
+	var projectHTML = '<a href="#" class="thumbnail">' + 
+	'<img src="' + result['image'] + '" class="detailsImage">' + '</a>' +
+	'<p>' + result['title'] + '</p>' +
+	'<p><small>' + result['date'] + '</small></p>' +
+	'<p>' + result['summary'] + '</p>';
+$('#project' + result['id'] + ' .details').html(projectHTML);
+}
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +50,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", changeColor);
+
+}
+function changeColor(result) {
+	console.log(result);
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+$('.thumbnail').css('background-color', colors[1]);
+$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+$('p').css('color', colors[3]);
+$('.project img').css('opacity', .75);
 }
